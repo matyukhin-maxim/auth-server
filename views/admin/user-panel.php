29eab65ed9b98a4c096ffdecfacb1775f000007c
fellix <matuhin.maxim@gmail.com>
2016-02-02 01:14:53 +0900

    +

--- views/admin/user-panel.php
+++ views/admin/user-panel.php
@@ -1,8 +1,8 @@
 <div class="col-sm-4">
 	<div class="panel panel-success">
 		<div class="panel-heading role-name strong title"><?= $fullname; ?></div>
-		<div class="panel-footer">
-			<div class="btn-group btn-group-justified">
+		<div class="panel-footer nmnp">
+			<div class="btn-group btn-group-xs btn-group-justified">
 				<a href="#" class="btn btn-default">11</a>
 				<a href="#" class="btn btn-default">22</a>
 				<a href="#" class="btn btn-danger" title="Удалить">
