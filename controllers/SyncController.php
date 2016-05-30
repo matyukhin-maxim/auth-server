a82f74262d8daaac94e3d46cc14982c19e613f2d
work <matuhin.maxim@gmail.com>
2016-05-30 17:16:53 +0900

    Синхронизация с телефонным справочником

---
+++ controllers/SyncController.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Матюхин_МП
+ * Date: 27.05.2016
+ * Time: 13:00
+ */
+class SyncController {
+
+}
---
+++ models/SyncModel.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Матюхин_МП
+ * Date: 27.05.2016
+ * Time: 15:40
+ */
+class SyncModel {
+
+}
