68a3852faec9132054845805a8593b3d02149f83
work <matuhin.maxim@gmail.com>
2016-05-30 17:16:15 +0900

    Немного оптимизации

--- core/Cipher.php
+++ core/Cipher.php
@@ -10,7 +10,7 @@ class Cipher {
 
 	public static $method = 'aes-256-ctr';
 
-	public static function encode($data, $secret, $serialize = false) {
+	public static function encode($data, $secret, $serialize = true) {
 
 		if ($serialize) $data = serialize($data);
 
@@ -18,37 +18,32 @@ class Cipher {
 			$data,
 			self::$method,
 			$secret,
-			OPENSSL_ZERO_PADDING
+			OPENSSL_ZERO_PADDING,
+			sha1('ngres')
 		);
 	}
 
-	public static function decode($data, $secret, $serialize = false) {
+	public static function decode($data, $secret, $serialize = true) {
 
 		$plain = openssl_decrypt(
 			$data,
 			self::$method,
 			$secret,
-			OPENSSL_ZERO_PADDING
+			OPENSSL_ZERO_PADDING,
+			sha1('ngres')
 		);
 
 		return $serialize ? unserialize($plain) : $plain;
 	}
 
-	public static function generate_token ($len = 32)
-	{
-		// Алфавит возможных символов
-		$chars = array(
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-		);
-		shuffle($chars);
-		$num_chars = count($chars) - 1;
+	public static function generate_token($len = 32) {
+
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		//$chars = str_shuffle($chars);
+		$nc = strlen($chars) - 1;
+
 		$token = '';
-		// Генерируем необходимое количество рндомных символов
-		for ($i = 0; $i < $len; $i++) $token .= $chars[mt_rand(0, $num_chars)];
+		for ($idx = 0; $idx < $len; $idx++) $token .= $chars[mt_rand(0, $nc)];
 		return $token;
 	}
 }
